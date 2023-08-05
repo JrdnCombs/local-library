ff9120a3378a7384328ccb36fda40c2dd35550ab
Jordan Combs <jrdncombs@gmail.com>
2023-08-05 09:17:19 -0500

    books.2&3

--- public/src/books.js
+++ public/src/books.js
@@ -2,9 +2,23 @@ function findAuthorById(authors, id) {
   return author = authors.find(author => author.id === id)
 }
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return book = books.find(book => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const checkedOutBooks = books.filter((book) => {
+    const [latestTransaction] = book.borrows;
+    return !latestTransaction.returned;
+  });
+
+  const returnedBooks = books.filter((book) => {
+    const [latestTransaction] = book.borrows;
+    return latestTransaction.returned;
+  });
+
+  return [checkedOutBooks, returnedBooks];
+}
 
 function getBorrowersForBook(book, accounts) {}
 
